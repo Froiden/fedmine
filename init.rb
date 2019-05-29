b731b7fce111d67ce83242fab85f5b07043cde4f
Pouyan Heyratpour <pouyan@janstun.com>
2019-05-29 18:40:34 +0430

    Change repo url
    Signed-off-by: Pouyan Heyratpour <pouyan@janstun.com>

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :fedmine do
 	description 'Fedmine flat theme with rtl support'
 	version '1.0.0'
 	requires_redmine :version_or_higher => '3.0.0'
-	url 'https://github.com/ghasedak/fedmine'
+	url 'https://github.com/pattack/fedmine'
 	author_url 'https://github.com/pouyanh'
 
 	settings(:partial => 'settings/fedmine')
